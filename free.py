c0d688868f78ca9f2e98b693f2843339a1def2aa
navaz0872v <navazzinazz@gmail.com>
2025-04-05 20:23:30 +0530

    Update free.py

--- free.py
+++ free.py
@@ -22,7 +22,7 @@ KEY_FILE = "keys.json"
 # Cooldown settings
 COOLDOWN_TIME = 0  # in seconds
 CONSECUTIVE_ATTACKS_LIMIT = 2
-CONSECUTIVE_ATTACKS_COOLDOWN = 240  # in seconds
+CONSECUTIVE_ATTACKS_COOLDOWN = 10  # in seconds
 
 # In-memory storage
 users = {}
